617fad1a167d04ecc06a94db286b245a72eac6e1
hhkk <hank.khang@gmail.com>
2019-08-05 04:14:24 -0400

    x

--- server/src/app.js
+++ server/src/app.js
@@ -18,5 +18,5 @@ hbk(app)
 sequelize.sync({force: false})
   .then(() => {
     app.listen(config.port)
-    console.log('============================== > hbk5 Server started on port:' + config.port) // 8081
+    console.log('============================== > hbk6 Server started on port:' + config.port) // 8081
   })
